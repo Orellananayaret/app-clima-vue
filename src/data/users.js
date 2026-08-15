3aca35e8f01ac8e83fa6bc63c2d20347212f3085
Nayaret Orellana <nayaret.orellanag@gmail.com>
2026-08-15 15:25:44 -0400

    Agregarusers.js

---
+++ src/data/users.js
@@ -0,0 +1,24 @@
+export const users = [
+  {
+    id: 1,
+    name: 'Nayaret',
+    email: 'nayaret@email.com',
+    password: '123456',
+    favorites: ['Santiago', 'Valparaíso'],
+    preferences: {
+      temperatureUnit: 'C',
+      theme: 'light'
+    }
+  },
+  {
+    id: 2,
+    name: 'Isidora',
+    email: 'isidora@email.com',
+    password: 'abcdef',
+    favorites: ['Concepción'],
+    preferences: {
+      temperatureUnit: 'F',
+      theme: 'dark'
+    }
+  }
+]
